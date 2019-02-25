210f64cec4545f6bb455a8ed61ca53d37baf3abb
dapperauteur <a@awews.com>
2019-02-25 09:15:26 -0700

    practicing context, not react-context, but what this is

--- lecture/context.js
+++ lecture/context.js
@@ -1,10 +1,31 @@
+const { getNameForPhonebook } = require("./context2");
+
 class Person {
   constructor(first, last) {
-    this.firstName = first
-    this.lastName = last
+    this.firstName = first;
+    this.lastName = last;
   }
 
   getFullName() {
-    return this.firstName + ' ' + this.lastName
+    return this.firstName + " " + this.lastName;
   }
 }
+
+function tellEmWhyYouMad(reasonWhyYoureMad) {
+  return reasonWhyYoureMad;
+}
+
+// add getNameForPhonebook function to Person Class
+Person.prototype.getNameForPhonebook = getNameForPhonebook;
+Person.prototype.tellEmWhyYouMad = tellEmWhyYouMad;
+
+const t$ = new Person("earl", "jones");
+const awe = new Person("awe", "ful");
+
+console.log("getNameForPhonebook.apply()", getNameForPhonebook.apply(t$));
+console.log("getNameForPhonebook.bind()", getNameForPhonebook.bind(t$));
+console.log("getNameForPhonebook.call()", getNameForPhonebook.call(t$));
+console.log(
+  "awe.tellEmWhyYouMad(my shit john blaze)",
+  awe.tellEmWhyYouMad("my shit john blaze")
+);
---
+++ src/Context.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import ContextChild from "./ContextChild";
+
+function myBind(obj, fn) {
+  return function() {
+    return fn.apply(obj, arguments);
+  };
+}
+
+export class Context extends Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+    this.onClick = myBind(this, this.onClick);
+  }
+
+  onClick() {
+    console.log("this", this);
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Context</h1>
+        <ContextChild onClick={this.onClick} />
+        <div>Counter: {this.state.counter}</div>
+      </div>
+    );
+  }
+}
+
+export default Context;
